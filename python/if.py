90f6488f6e7be5609c0c1ab43d91db375d29301a
sincein1 <sincein1@naver.com>
2022-02-07 16:16:44 +0900

    2022.02.07

---
+++ python/if.py
@@ -0,0 +1,80 @@
+# from turtle import Turtle
+
+
+# money = True
+# if money:
+#     print("택시타고가라")
+# else:
+#     print("걸어가라")
+
+# money = 2000
+# card = True
+# if money >= 3000 or card:
+#     print("택시타라")
+# else:
+#     print("걸어라")
+
+# pocket = ['paper', 'money', 'phone']
+# card = True
+# if 'money' in pocket:
+#     print('택시')
+# elif card:
+#     print('택시')
+# else:
+#     print('걸어')
+
+# pocket = ['money', 'phone']
+# if 'money' in pocket:
+#     pass
+# else:
+#     pass
+
+# score = 50
+# if score >= 60:
+#     message = 'success'
+# else:
+#     message = 'failure'
+    
+# print(message)
+
+# mess = 'success' if score >=60 else 'failure'
+
+# print(mess)
+
+# pocket = ["money", "phone"]
+
+# print("택시" if "money" or "card" in pocket else "걸어라")
+
+# treehit = 0
+# while treehit < 10:
+#     treehit = treehit +1
+#     print(f"나무를 {treehit}번 찍었습니다.")
+#     if treehit == 10:
+#         print("나무 넘어갑니다.")
+
+# prompt = """
+#     1. Add
+#     2. Del
+#     3. List
+#     4. Quit
+    
+#     Enter number: """
+
+# number = 0
+# while number != 4:
+#     print(prompt)
+#     number = int(input())
+
+# coffee = 10
+# while True:
+#     money = int(input("돈을 넣어주세요: "))
+#     if money >= 300:
+#         print("커피를 줍니다.")
+#         coffee = coffee -1
+#         print(f"거스름돈 {money-300} 입니다.")
+#         print(f"남은 커피는 {coffee}개 입니다.")
+#     else:
+#         print("돈을 돌려주고 커피를 주지 않습니다.")
+#     if coffee == 0:
+#         print("끝")
+#         break
